1c5333ec03d25db39943c648016425570bd2ba25
Dana Wensberg <dana.wensberg@gmail.com>
2023-02-25 13:39:36 -0500

    cleaned up docker files and docker compose to get the app finally working and running. Up next is the login flow, and then we will be off to the races

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -9,6 +9,7 @@ echo "Waiting for db to be ready..."
 ./manage.py migrate
 sleep 2
 
+# TODO: figure out static file deployment with nginx
 ./manage.py collectstatic --noinput
 
-gunicorn -c /app/docker/webservices/gunicorn.conf.py webservices.wsgi
+gunicorn -c /app/docker/backend/gunicorn.conf.py webservices.wsgi

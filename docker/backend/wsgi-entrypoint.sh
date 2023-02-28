bbd6c59949043aa51c460cf11671a6e0d3526f38
Dana Wensberg <dana.wensberg@gmail.com>
2023-02-28 13:24:13 -0500

    commenting out call to migrate in entrpyopint, will do that manually.

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 echo "Waiting for server volume..."
 cd /app/webservices
 
-echo "Waiting for db to be ready..."
-./manage.py migrate
-sleep 2
+# echo "Waiting for db to be ready..."
+# ./manage.py migrate
+# sleep 2
 
-# TODO: figure out static file deployment with nginx
-./manage.py collectstatic --noinput
+# # TODO: figure out static file deployment with nginx
+# ./manage.py collectstatic --noinput
 
 gunicorn -c /app/docker/backend/gunicorn.conf.py webservices.wsgi
